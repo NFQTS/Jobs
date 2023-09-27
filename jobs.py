ba5e35b72e61215a940984ca9f20756afbc3b363
Jerry <94505747+NFQTS@users.noreply.github.com>
2023-09-26 19:42:13 -0700

    Create jobs.py

---
+++ jobs.py
@@ -0,0 +1,10 @@
+# Need to match people to jobs
+# Filtering based on interest, skill, and depth of skill makes sense
+# Need a differentiator but... that should be easy due to the way I run things we are already different by being proactive.
+# Will probably need a neural network or adapt a recommendation system to it.
+# May have to get wild and assign scores to each job for each candidate based on tons of metrics but idk.
+
+job_sites = ["LinkedIn", "GitHub", "Indeed", "Monster Jobs", "GlassDoor", "Dice Jobs", "SimplyHired", "Seekr", "Stack Overflow", "CareerJet", "Adzuna", "Neuvoo",
+            "Authentic Jobs", "We Work Remotely", "Remote OK"]
+
+api_dict = {}
