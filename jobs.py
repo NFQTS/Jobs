b8ee0ace755054c8f8a8d2466d5bf55018755719
Jerry <94505747+NFQTS@users.noreply.github.com>
2023-09-28 13:33:50 -0700

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -4,7 +4,11 @@
 # Will probably need a neural network or adapt a recommendation system to it.
 # May have to get wild and assign scores to each job for each candidate based on tons of metrics but idk.
 
-job_sites = ["LinkedIn", "GitHub", "Indeed", "Monster Jobs", "GlassDoor", "Dice Jobs", "SimplyHired", "Seekr", "Stack Overflow", "CareerJet", "Adzuna", "Neuvoo",
-            "Authentic Jobs", "We Work Remotely", "Remote OK"]
+#job_sites = ["LinkedIn", "GitHub", "Indeed", "Monster Jobs", "GlassDoor", "Dice Jobs", "SimplyHired", "Seekr", "Stack Overflow", "CareerJet", "Adzuna", "Neuvoo",
+            #"Authentic Jobs", "We Work Remotely", "Remote OK"]
 
-api_dict = {}
+#api_dict = {}
+
+# Seems like a lot of these services have restricted API access, most likely to prevent losing placement fees.
+# Best bet to start with seems like the google cloud talent solutions (cant remember name, its something like that)
+# Google is a little confusing to connect to so going to have to scope documentation to figure it out.
